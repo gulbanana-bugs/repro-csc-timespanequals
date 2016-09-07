af39a4c922b9dd7baea6a0dc403cdf532ec87ff3
Thomas Castiglione <thomas@agcom.com.au>
2016-09-07 15:59:15 +0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace repro_csc_timespanequals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan t = TimeSpan.FromSeconds(1);
+
+            DidIGetTheTime(t); // no problem
+            DidIGetTheTime();  // also no problem, somehow?
+        }
+
+        static void DidIGetTheTime(TimeSpan t = default(TimeSpan))
+        {
+            if (t == null) // whoops!
+            {
+                throw new ArgumentOutOfRangeException("no TimeSpan provided!");
+            }
+        }
+    }
+}
